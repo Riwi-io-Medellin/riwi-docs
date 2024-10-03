3ea5c8dae25fff1f2b26055980b383541a503e65
Kevin Mejia <kwmejia9@gmail.com>
2024-10-03 11:51:21 -0500

    feat/add structure proyects

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import useAuth from '../hooks/use-auth';
 
 export default function Root({ children }) {
-  const loading = useAuth();  // Obtiene el estado de carga desde el hook
+  const loading = useAuth();
 
   if (loading) {
-    // Puedes mostrar un spinner o pantalla de carga aquí
+
     return <div>Cargando...</div>;
   }
 
-  // Solo renderiza los hijos si no está cargando (autenticación ya verificada)
   return <>{children}</>;
 }
